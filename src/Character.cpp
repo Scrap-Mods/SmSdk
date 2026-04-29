611a8fc03837dbe56eaf0558b44de83373548fdb
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2026-04-29 12:32:12 -0700

    Character improvements

---
+++ src/Character.cpp
@@ -0,0 +1,63 @@
+#include "SmSdk/Character.hpp"
+
+#include "SmSdk/Physics/CharacterPhysicsProxy.hpp"
+
+SMSDK_BEGIN_NAMESPACE
+
+std::uint16_t Character::getWorldId() const
+{
+	return m_uWorldId;
+}
+
+std::uint64_t Character::getOwnerSteamId() const
+{
+	return m_uOwnerSteamId;
+}
+
+std::uint32_t Character::getUnitId() const
+{
+	return m_uUnitId;
+}
+
+boost::uuids::uuid Character::getUuid() const
+{
+	return m_uuid;
+}
+
+boost::uuids::uuid Character::getSelectedToolUuid() const
+{
+	return m_selectedToolUuid;
+}
+
+DirectX::XMFLOAT4 Character::getColor() const
+{
+	return m_color;
+}
+
+std::string_view Character::getNameTag() const
+{
+	return m_nameTag;
+}
+
+DirectX::XMFLOAT3 Character::getPosition() const
+{
+	const auto v_pos = m_pPhysicsProxy->m_pTickRaycastCollisionObject->getWorldTransform().getOrigin();
+	return DirectX::XMFLOAT3(v_pos.x(), v_pos.y(), v_pos.z());
+}
+
+DirectX::XMFLOAT3 Character::getVelocity() const
+{
+	return m_velocity;
+}
+
+float Character::getPitch() const
+{
+	return m_fPitch;
+}
+
+float Character::getYaw() const
+{
+	return m_fYaw;
+}
+
+SMSDK_END_NAMESPACE
